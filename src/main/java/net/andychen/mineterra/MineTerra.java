7f2e857b421f8ac408b629dbd8044c97bb44bbb9
Andy Chen <andyc19@uci.edu>
2022-11-13 02:25:46 -0800

    Initial commit

---
+++ src/main/java/net/andychen/mineterra/MineTerra.java
@@ -0,0 +1,14 @@
+package net.andychen.mineterra;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MineTerra implements ModInitializer {
+	public static final String MOD_ID = "mineterra";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
